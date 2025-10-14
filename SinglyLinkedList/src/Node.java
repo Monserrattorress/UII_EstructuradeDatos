032529589030ab67b247e289003357cb4f893756
Monserrattorress <20243ds147@utez.edu.mx>
2025-10-14 08:57:44 -0600

    Add files via upload

---
+++ SinglyLinkedList/src/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null; //opcional pero no esta mal que lo pongas para
+        //que no se te olvide que la referencia al instanciarlo es null
+        
+    }
+}
